1b79d8283d8ae460931fcd09d608bd1664a14859
alanatilio <alanarapa15@gmail.com>
2022-10-21 22:52:18 -0500

    limpiar todo lo que no se usa

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,45 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-
+//codigo js para el enfoque de las imagenes
 $(document).ready(function() {
   AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
+  // elimine el comentario a continuación para que las animaciones en desplazamiento se reproduzcan solo una vez
+  
+  }); // inicializa la animación en la biblioteca de desplazamiento
 });
 
-// Smooth scroll for links with hashes
+// Desplazamiento suave para enlaces con hash
 $('a.smooth-scroll')
 .click(function(event) {
-  // On-page links
+  // Enlaces en la página
   if (
     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
     && 
     location.hostname == this.hostname
   ) {
-    // Figure out element to scroll to
+    // Averiguar el elemento al que desplazarse
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
+    // ¿Existe un objetivo de desplazamiento?
     if (target.length) {
-      // Only prevent default if animation is actually gonna happen
+      // Solo evita el valor predeterminado si la animación realmente va a suceder
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top
       }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
+        // Devolución de llamada después de la animación
+        // ¡Debe cambiar el enfoque!
         var $target = $(target);
         $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
+        if ($target.is(":focus")) { // Comprobando si el objetivo estaba enfocado
           return false;
         } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
+          $target.attr('tabindex','-1'); // Agregar tabindex para elementos no enfocables
+          $target.focus(); // Poner foco de nuevo
         };
       });
     }
   }
 });
-
+/*
 $.ajax({
   type:'POST',
   url:'EnviarForm.php',
@@ -62,4 +60,59 @@ $.ajax({
       $('.submitBtn').removeAttr("disabled");
       $('.modal-body').css('opacity', '');
   }
-});
+});*/
+
+//ENVIO DE FORMUALRIO DE CONTACTO USANDO UNA WEB "formspree"
+//api que devuelbe 200 ok si es que el formulario se envi correctamente
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  if (response.ok) {
+    this.reset()
+    alert('Gracias por contactarme, te escribiré pronto')
+  }
+}
+
+
+//BTC USD
+//codigo para hacer usu de la API de coindesk para el precio de BTC
+const dollarContainer = document.getElementById('dollar');
+const usdAmount = document.getElementById('usd-amount');
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => response.json())
+    .then(data => displayData(data));
+
+const displayData = data => {
+    const usd = data.bpi.USD.rate_float;
+    usdAmount.textContent = `$${usd} USD`;
+    const totalDollarItems = Math.trunc(usd / 1000);
+    for(let i = 0; i < totalDollarItems; i++) {
+        const newDollar = document.createElement('div');
+        newDollar.setAttribute("style", `animation-delay:.${10 + i}s;`);
+        newDollar.textContent = '$';
+        newDollar.setAttribute('class', 'coin dollar-item');
+        dollarContainer.appendChild(newDollar);
+    } 
+}
+
+
+//FOOTER
+/*
+function mostrarFooter(name) {
+      copyright = new Date();
+      update = copyright.getFullYear();
+      document.write(" © Sorian " + update + "   By  " + name);
+}*/
