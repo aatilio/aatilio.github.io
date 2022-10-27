429bfc52d46e959ec3707b69031db7cfce1b96cb
alanatilio <alanarapa15@gmail.com>
2022-10-27 01:02:59 -0500

    comentarios

--- crypto/script.js
+++ crypto/script.js
@@ -1,10 +1,11 @@
+//SELECTORES
 const form = document.querySelector("#form-search");
 const moneda = document.querySelector("#moneda");
 const criptomoneda = document.querySelector("#criptomonedas");
 const formContainer = document.querySelector(".form-side");
 const containerAnswer = document.querySelector(".container-answer");
 
-const objBusqueda = {
+const objBusqueda = { //objeto de las opciones
     moneda: '',
     criptomoneda: ''
 }
@@ -12,13 +13,13 @@ const objBusqueda = {
 //cada ves que se carga la pagina
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptos();
-
     form.addEventListener('submit', submitForm);
     moneda.addEventListener('change', getValue);
     criptomoneda.addEventListener('change', getValue);
 })
 
-function submitForm(e){
+//valida que los capos esten seleccionados, no podemos llamar a la API 
+function submitForm(e){//Le llega como parameto un evento
     e.preventDefault();
     const {moneda, criptomoneda} = objBusqueda;
     if (moneda === '' || criptomoneda === '') {
@@ -30,18 +31,19 @@ function submitForm(e){
     //console.log(criptomoneda);
 }
 
+//principal
 function consultarAPI(moneda, criptomoneda){
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
     fetch(url)
         .then(resultado => resultado.json())
-        .then(resultadoJson => {
+        .then(resultadoJson => { //de la respuesta pasamos el atributo data con los 10 objetos
             mostrarCotizacion(resultadoJson.DISPLAY[criptomoneda][moneda]);
             //console.log(resultadoJson.DISPLAY[criptomoneda][moneda]);
         })
         .catch(error => console.log(error));
 }
 
-//escojemos lo que vamos a utilizar
+//escojemos lo que vamos a utilizar, estructuracion
 function mostrarCotizacion(data){
     clearHTML();
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = data;
@@ -54,9 +56,9 @@ function mostrarCotizacion(data){
         <p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span></p>
         <p>Última Actualización: <span>${LASTUPDATE}</span></p>
     `;
-    containerAnswer.appendChild(answer);
+    containerAnswer.appendChild(answer);//le pasamos lo que hemos seteado al selector para que ointe en la pantalla 
 }
-
+//si es que nos sale un error al consultar la api
 function showError(mensage){
     const error = document.createElement('p');
     error.classList.add("error");
@@ -64,11 +66,12 @@ function showError(mensage){
     formContainer.appendChild(error);
     setTimeout(() => error.remove(), 3000);
 }
-
+//del objeto busqueda el atributo moneda setee al valor de la crypto
 function getValue(e){
     objBusqueda[e.target.name] = e.target.value; 
 }
 
+//para la opcion de escojer que coin es la que vamos a escojer
 function consultarCriptos(){
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
     
@@ -81,16 +84,17 @@ function consultarCriptos(){
         .catch(error => console.log(error));
 }
 
+
 function selectCriptos(criptos){
     criptos.forEach(cripto => {
         const {FullName, Name} = cripto.CoinInfo;
-        const option = document.createElement("option");
-        option.value = Name;
-        option.textContent = FullName;
-        criptomoneda.appendChild(option);
+        const option = document.createElement("option");//elemento de tipo obcion para setear
+        option.value = Name; //le ponemos un valor
+        option.textContent = FullName; //contenido que va a terner 
+        criptomoneda.appendChild(option); //le pasamos los valores a las opciones de coins
     });
 }
-
+//limpia el html que se pinta en cada recarga
 function clearHTML(){
     containerAnswer.innerHTML = '';
 }
